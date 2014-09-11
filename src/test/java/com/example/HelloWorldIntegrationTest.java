30ff77ef64cf57152e696c0ef179c33166487648
pecampbell <pcampbell@C1MMN25DDV30.local>
2014-09-11 15:48:02 -0500

    Make modifications to test btrace instrumentation

--- src/test/java/com/example/HelloWorldIntegrationTest.java
+++ src/test/java/com/example/HelloWorldIntegrationTest.java
@@ -66,7 +66,7 @@ public class HelloWorldIntegrationTest {
 		HomePage homePage = new HomePage(driver);
 		HelloWorldPage helloWorldPage = homePage.clickMessageLink();
 		
-		for(int i=0; i < 100; i++){
+		for(int i=0; i < 1; i++){
 			assertEquals("Hello world",helloWorldPage.getMessage());
 		}
 	}
